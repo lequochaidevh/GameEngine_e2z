ea075912c8c93e70434d4f0c0f2fed7c0a67a309
DevH <lequochaidevh@gmail.com>
2024-11-01 12:45:34 +0700

    test performance memory-allocator-method

--- Game/Source/GamePlayLayer.h
+++ Game/Source/GamePlayLayer.h
@@ -5,6 +5,7 @@
 #include "Core/Layer/LayerStack.h"
 #include "Core/Logger/Logger.h"
 #include "Core/Memory/MemoryAllocator.h"
+#include<chrono>
 
 #define POOL_MEMORY_ALLOCATOR
 
@@ -16,6 +17,7 @@
 
 void testCaseMemoryAllocator(VIEngine::MemoryAllocator* mAllocator);
 void testCaseMemoryPoolAllocator(VIEngine::PoolAllocator* mAllocator);
+void testPerformanceMemoryAllocator();
 
 class GameplayLayer : public VIEngine::Layer {
 public:
@@ -23,22 +25,23 @@ public:
 	~GameplayLayer() {}
 	virtual void onAttach() override {
 		LOG_TRACE("GameplayLayer is attached");
-		#ifdef POOL_MEMORY_ALLOCATOR
-			testCaseMemoryPoolAllocator(mAllocator);
-		#elif defined(LINEAR_MEMORY_ALLOCATOR) || defined(STACK_MEMORY_ALLOCATOR)
-			testCaseMemoryAllocator(mAllocator);
-		#endif // POOL_ALLOCATOR
+		testPerformanceMemoryAllocator();
+		//#ifdef POOL_MEMORY_ALLOCATOR
+		//	testCaseMemoryPoolAllocator(mAllocator);
+		//#elif defined(LINEAR_MEMORY_ALLOCATOR) || defined(STACK_MEMORY_ALLOCATOR)
+		//	testCaseMemoryAllocator(mAllocator);
+		//#endif // POOL_ALLOCATOR
 		
 	}
 	virtual void onDetach() override {
 		LOG_TRACE("GameplayLayer is detached");
 	}
 	virtual bool onKeyPressedEvent(const VIEngine::KeyPressedEvent& eventContext) override {
-		LOG_TRACE("Key {0} is pressed", (char)eventContext.getKey());
+		//LOG_TRACE("Key {0} is pressed", (char)eventContext.getKey());
 		return false;
 	}
 	virtual void onUpdate(VIEngine::Time time) {
-		LOG_TRACE("{0} Delta Time", time.getDeltaTime());
+		//LOG_TRACE("{0} Delta Time", time.getDeltaTime());
 	}
 	
 
@@ -148,4 +151,134 @@ void testCaseMemoryPoolAllocator(VIEngine::PoolAllocator* mAllocator) {
 
 	mAllocator->memClear();
 	gameObjects.clear();
+}
+
+void testPerformanceMemoryAllocator() {
+	VIEngine::LinearAllocator* mLinearAllocator;
+	VIEngine::StackAllocator* mStackAllocator;
+	VIEngine::PoolAllocator* mPoolAllocator;
+	size_t size = 128 * 1024 * 1024;
+
+	struct GameObject {
+		size_t ID = 0;
+		std::string Name = "GameObject";
+	};
+
+	mLinearAllocator = new VIEngine::LinearAllocator(size, malloc(size));
+	mStackAllocator = new VIEngine::StackAllocator(size, malloc(size));
+	mPoolAllocator = new VIEngine::PoolAllocator(size, malloc(size), sizeof(GameObject), alignof(GameObject));
+
+	int numOfObjects = 2000000;
+	std::vector<GameObject*> objects;
+
+	// NEW and DELETE operator
+	auto startTime = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < numOfObjects; i++) {
+		GameObject* go = new GameObject();
+		go->ID = i;
+		objects.emplace_back(go);
+	}
+
+	for (GameObject* object : objects) {
+		VI_FREE_MEMORY(object);
+	}
+
+	objects.clear();
+
+	for (int i = 0; i < numOfObjects; i++) {
+		GameObject* go = new GameObject();
+		go->ID = i;
+		objects.emplace_back(go);
+	}
+
+	for (GameObject* object : objects) {
+		VI_FREE_MEMORY(object);
+	}
+
+	objects.clear();
+
+	auto endTime = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double, std::milli> duration = endTime - startTime;
+	CORE_LOG_INFO("Delete operator takes: {0} seconds", duration.count() / 1000.0f);
+
+	// LINEAR ALLOCATOR
+	startTime = std::chrono::high_resolution_clock::now();
+
+	for (int i = 0; i < numOfObjects; i++) {
+		void* memory = mLinearAllocator->memAllocate(sizeof(GameObject), alignof(GameObject));
+		GameObject* go = new (memory)GameObject();
+		go->ID = i;
+		objects.emplace_back(go);
+	}
+
+	mLinearAllocator->memClear();
+	objects.clear();
+
+	for (int i = 0; i < numOfObjects; i++) {
+		void* memory = mLinearAllocator->memAllocate(sizeof(GameObject), alignof(GameObject));
+		GameObject* go = new (memory)GameObject();
+		go->ID = i;
+		objects.emplace_back(go);
+	}
+
+	mLinearAllocator->memClear();
+	objects.clear();
+
+	endTime = std::chrono::high_resolution_clock::now();
+	duration = endTime - startTime;
+	CORE_LOG_INFO("LinearAllocator takes: {0} seconds", duration.count() / 1000.0f);
+
+	// STACK ALLOCATOR
+	startTime = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < numOfObjects; i++) {
+		void* memory = mStackAllocator->memAllocate(sizeof(GameObject), alignof(GameObject));
+		GameObject* go = new (memory)GameObject();
+		go->ID = i;
+		objects.emplace_back(go);
+	}
+
+	mStackAllocator->memClear();
+	objects.clear();
+
+	for (int i = 0; i < numOfObjects; i++) {
+		void* memory = mStackAllocator->memAllocate(sizeof(GameObject), alignof(GameObject));
+		GameObject* go = new (memory)GameObject();
+		go->ID = i;
+		objects.emplace_back(go);
+	}
+
+	mStackAllocator->memClear();
+	objects.clear();
+
+	endTime = std::chrono::high_resolution_clock::now();
+	duration = endTime - startTime;
+	CORE_LOG_INFO("StackAllocator takes: {0} seconds", duration.count() / 1000.0f);
+
+	// Pool ALLOCATOR
+	startTime = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < numOfObjects; i++) {
+		void* memory = mPoolAllocator->allocateChunk();
+		GameObject* go = new (memory)GameObject();
+		go->ID = i;
+		objects.emplace_back(go);
+	}
+
+	mPoolAllocator->memClear();
+	objects.clear();
+
+	for (int i = 0; i < numOfObjects; i++) {
+		void* memory = mPoolAllocator->allocateChunk();
+		GameObject* go = new (memory)GameObject();
+		go->ID = i;
+		objects.emplace_back(go);
+	}
+
+	mPoolAllocator->memClear();
+	objects.clear();
+
+	endTime = std::chrono::high_resolution_clock::now();
+	duration = endTime - startTime;
+	CORE_LOG_INFO("PoolAllocator takes: {0} seconds", duration.count() / 1000.0f);
+
+	CORE_LOG_TRACE("DONE TESTING MEMROY ALLOCATOR");
 }
